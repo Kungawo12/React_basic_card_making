2021c082408f5022bb85bc3969389f51a3ad7655
Tenzin Kunga <officialtenzin.kunga@gmail.com>
2025-01-12 15:52:57 -0600

    mkae some changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <Avatar 
+      img = "https://media.licdn.com/dms/image/v2/D5603AQHcvv1E3t-QbA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1711396184384?e=1742428800&v=beta&t=IAj8L6Qt-W-7-YaUGUGyuLNNJjUKFVFGAMdv7zvrYSE"
+      />
       {contacts.map((contact,index) =>(
       <Card 
         key= {index}
