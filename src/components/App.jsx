60f4d0b3e46b6020722ed59dcc24d5a922da2483
Tenzin Kunga <officialtenzin.kunga@gmail.com>
2025-01-12 16:42:43 -0600

    updated some and done some experiment with id creation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,19 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./Avatar";
 
+function createCared(contact){
+  return(
+    <Card 
+      id = {contact.id}
+      key = {contact.id}
+      name = {contact.name}
+      img = {contact.imgURL}
+      tel = {contact.phone}
+      email = {contact.email}
+    />
+  )
+}
+
 function App() {
   return (
     <div>
@@ -10,7 +23,9 @@ function App() {
       <Avatar 
       img = "https://media.licdn.com/dms/image/v2/D5603AQHcvv1E3t-QbA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1711396184384?e=1742428800&v=beta&t=IAj8L6Qt-W-7-YaUGUGyuLNNJjUKFVFGAMdv7zvrYSE"
       />
-      {contacts.map((contact,index) =>(
+      {contacts.map(createCared)}
+
+      {/* {contacts.map((contact,index) =>(
       <Card 
         key= {index}
         name = {contact.name}
@@ -18,7 +33,7 @@ function App() {
         tel = {contact.phone}
         email = {contact.email}
       />
-      ))}
+      // ))} */}
     </div>
   );
 }
